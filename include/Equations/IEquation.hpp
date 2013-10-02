61a3388568e84434772512aa2d53418230a0072a
Philippe Marti <philippe.marti@colorado.edu>
2013-10-02 10:14:17 -0600

    Reduced code in IEquation through templates

--- include/Equations/IEquation.hpp
+++ include/Equations/IEquation.hpp
@@ -34,6 +34,21 @@ namespace GeoMHDiSCC {
 
 namespace Equations {
 
+   namespace internal
+   {
+      void addExplicitWrapper(Matrix& rEqField, const int eqStart, const SparseMatrix& mat, const Eigen::Ref<const Matrix>& rhs);
+
+      template <typename TData> void addExplicitWrapper(TData& rEqField, const int eqStart, const SparseMatrix& mat, const Eigen::Ref<const MatrixZ>& rhs);
+
+      void addExplicitWrapper(MatrixZ& rEqField, const int eqStart, const SparseMatrixZ& mat, const Eigen::Ref<const MatrixZ>& rhs);
+
+      void addExplicitWrapper(DecoupledZMatrix& rEqField, const int eqStart, const SparseMatrixZ& mat, const Eigen::Ref<const MatrixZ>& rhs);
+
+      template <typename TData> void applyQuasiInverseWrapper(TData& rField, const int start, const int rows, const SparseMatrix& mat);
+
+      template <> void applyQuasiInverseWrapper<DecoupledZMatrix>(DecoupledZMatrix& rField, const int start, const int rows, const SparseMatrix& mat);
+   }
+
    /**
     * @brief Base building block for the implementation of an equation
     */
@@ -174,8 +189,7 @@ namespace Equations {
     * @param matIdx  Index of the given data
     * @param start   Start index for the storage
     */
-   void applyQuasiInverse(const IEquation& eq, FieldComponents::Spectral::Id compId, DecoupledZMatrix& storage, const int matIdx, const int start);
-   void applyQuasiInverse(const IEquation& eq, FieldComponents::Spectral::Id compId, MatrixZ& storage, const int matIdx, const int start);
+   template <typename TData> void applyQuasiInverse(const IEquation& eq, FieldComponents::Spectral::Id compId, TData& storage, const int matIdx, const int start);
 
    /**
     * @brief Compute and add the explicit linear terms
@@ -187,8 +201,7 @@ namespace Equations {
     * @param explicitField   Explicit linear field values
     * @param matIdx     System index
     */
-   void addExplicitLinear(const IEquation& eq, FieldComponents::Spectral::Id compId, DecoupledZMatrix& eqField, const int eqStart, SpectralFieldId fieldId, const Datatypes::SpectralScalarType& explicitField, const int matIdx);
-   void addExplicitLinear(const IEquation& eq, FieldComponents::Spectral::Id compId, MatrixZ& eqField, const int eqStart, SpectralFieldId fieldId, const Datatypes::SpectralScalarType& explicitField, const int matIdx);
+   template <typename TData> void addExplicitLinear(const IEquation& eq, FieldComponents::Spectral::Id compId, TData& eqField, const int eqStart, SpectralFieldId fieldId, const Datatypes::SpectralScalarType& explicitField, const int matIdx);
 
    /**
     * @brief Dummy implementation: This should never be called!
@@ -209,6 +222,155 @@ namespace Equations {
     * @brief Dummy implementation. This should never get called!
     */
    void boundaryBlock(const IEquation& eq, FieldComponents::Spectral::Id compId, DecoupledZSparse& mat, const SpectralFieldId fieldId, const MHDFloat k); //= 0;
+
+   namespace internal
+   {
+      inline void addExplicitWrapper(Matrix& rEqField, const int eqStart, const SparseMatrix& mat, const Eigen::Ref<const Matrix>& rhs)
+      {
+         rEqField.block(eqStart, 0, mat.rows(), rEqField.cols()) += mat*rhs;
+      }
+
+      template <typename TData> inline void addExplicitWrapper(TData& rEqField, const int eqStart, const SparseMatrix& mat, const Eigen::Ref<const MatrixZ>& rhs)
+      {
+         int rows = mat.rows();
+         int cols = rEqField.real().cols();
+         rEqField.real().block(eqStart, 0, rows, cols) += mat*rhs.real();
+         rEqField.imag().block(eqStart, 0, rows, cols) += mat*rhs.imag();
+      }
+
+      inline void addExplicitWrapper(MatrixZ& rEqField, const int eqStart, const SparseMatrixZ& mat, const typename Eigen::Ref<const MatrixZ>& rhs)
+      {
+         rEqField.block(eqStart, 0, mat.rows(), rEqField.cols()) += mat*rhs;
+      }
+
+      inline void addExplicitWrapper(DecoupledZMatrix& rEqField, const int eqStart, const SparseMatrixZ& mat, const Eigen::Ref<const MatrixZ>& rhs)
+      {
+         assert(rEqField.real().rows() == rEqField.imag().rows());
+         assert(rEqField.real().cols() == rEqField.imag().cols());
+
+         int rows = mat.rows();
+         int cols = rEqField.real().cols();
+         rEqField.real().block(eqStart, 0, rows, cols) += mat.real()*rhs.real() - mat.imag()*rhs.imag();
+         rEqField.imag().block(eqStart, 0, rows, cols) += mat.real()*rhs.imag() + mat.imag()*rhs.real();
+      }
+
+      template <typename TData> inline void applyQuasiInverseWrapper(TData& rField, const int start, const int rows, const SparseMatrix& mat)
+      {
+         int cols = rField.cols();
+         rField.block(start, 0, rows, cols) = mat*rField.block(start, 0, rows, cols);
+      }
+
+      template <> inline void applyQuasiInverseWrapper<DecoupledZMatrix>(DecoupledZMatrix& rField, const int start, const int rows, const SparseMatrix& mat)
+      {
+         assert(rField.real().rows() == rField.imag().rows());
+         assert(rField.real().cols() == rField.imag().cols());
+
+         int cols = rField.real().cols();
+         rField.real().block(start, 0, rows, cols) = mat*rField.real().block(start, 0, rows, cols);
+         rField.imag().block(start, 0, rows, cols) = mat*rField.imag().block(start, 0, rows, cols);
+      }
+   }
+
+   template <typename TData> void applyQuasiInverse(const IEquation& eq, FieldComponents::Spectral::Id compId, TData& storage, const int matIdx, const int start)
+   {
+      // Apply quasi inverse
+      if(eq.couplingInfo(compId).hasQuasiInverse())
+      {
+         // Create pointer to sparse operator
+         const SparseMatrix * op = &eq.quasiInverse(compId, matIdx);
+
+         // Get number of rows
+         int rows = eq.couplingInfo(compId).blockN(matIdx);
+
+         // Safety asserts
+         assert(op->rows() == op->cols());
+         assert(op->cols() == rows);
+
+         // Multiply nonlinear term by quasi-inverse
+         internal::applyQuasiInverseWrapper(storage, start, rows, *op);
+      }
+   }
+
+   template <typename TData> void addExplicitLinear(const IEquation& eq, FieldComponents::Spectral::Id compId, TData& eqField, const int eqStart, SpectralFieldId fieldId, const Datatypes::SpectralScalarType& explicitField, const int matIdx)
+   {
+      // Compute with complex linear operator
+      if(eq.hasExplicitZLinear(compId, fieldId))
+      {
+         // Create pointer to sparse operator
+         const SparseMatrixZ * op = &eq.explicitZLinear(compId, fieldId, matIdx);
+
+         if(eq.couplingInfo(compId).indexType() == CouplingInformation::SLOWEST)
+         {
+            /// \mhdBug very bad and slow implementation!
+            Eigen::Matrix<Datatypes::SpectralScalarType::PointType,Eigen::Dynamic,1>  tmp(op->rows());
+            int k = 0;
+            for(int j = 0; j < explicitField.slice(matIdx).cols(); j++)
+            {
+               for(int i = 0; i < explicitField.slice(matIdx).cols(); i++)
+               {
+                  // Copy slice into flat array
+                  tmp(k) = explicitField.point(i,j,matIdx);
+
+                  // increase storage counter
+                  k++;
+               }
+            }
+
+            // Apply operator to field
+            internal::addExplicitWrapper(eqField, eqStart, *op, tmp);
+
+         } else if(eq.couplingInfo(compId).indexType() == CouplingInformation::MODE)
+         {
+            // Get mode indexes
+            ArrayI mode = eq.spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->mode(matIdx);
+
+            // Assert correct sizes
+            assert(op->rows() == explicitField.slice(mode(0)).rows());
+
+            // Apply operator to field
+            internal::addExplicitWrapper(eqField, eqStart, *op, explicitField.slice(mode(0)).col(mode(1)));
+         }
+      }
+
+      // Compute with real linear operator
+      if(eq.hasExplicitDLinear(compId, fieldId))
+      {
+         // Create pointer to sparse operator
+         const SparseMatrix * op = &eq.explicitDLinear(compId, fieldId, matIdx);
+
+         if(eq.couplingInfo(compId).indexType() == CouplingInformation::SLOWEST)
+         {
+            /// \mhdBug very bad and slow implementation!
+            Eigen::Matrix<Datatypes::SpectralScalarType::PointType,Eigen::Dynamic,1>  tmp(op->rows());
+            int k = 0;
+            for(int j = 0; j < explicitField.slice(matIdx).cols(); j++)
+            {
+               for(int i = 0; i < explicitField.slice(matIdx).cols(); i++)
+               {
+                  // Copy slice into flat array
+                  tmp(k) = explicitField.point(i,j,matIdx);
+
+                  // increase storage counter
+                  k++;
+               }
+            }
+
+            // Apply operator to field
+            internal::addExplicitWrapper(eqField, eqStart, *op, tmp);
+
+         } else if(eq.couplingInfo(compId).indexType() == CouplingInformation::MODE)
+         {
+            // Get mode indexes
+            ArrayI mode = eq.spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->mode(matIdx);
+
+            // Assert correct sizes
+            assert(op->rows() == explicitField.slice(mode(0)).rows());
+
+            // Apply operator to field: Re(eq) += op*Re(lin), Im(eq) += op*Im(lin)
+            internal::addExplicitWrapper(eqField, eqStart, *op, (*op)*explicitField.slice(mode(0)).col(mode(1)));
+         }
+      }
+   }
    
 }
 }
