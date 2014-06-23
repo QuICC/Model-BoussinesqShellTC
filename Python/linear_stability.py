36e116e3fd08d47247d505f0e0df22f15631abf2
Philippe Marti (garulfo) <philippe.marti@colorado.edu>
2014-06-22 23:08:24 -0600

    Apparently working python version of sptarn

--- Python/linear_stability.py
+++ Python/linear_stability.py
@@ -182,7 +182,7 @@ def sptarn(A, B, lb, ub, tolconv = 100*np.spacing(1), jmax = 100, maxmul = 10):
       t = B*np.random.rand(n,1)
       t = t/nplin.norm(t,np.inf)
       v = F(t)
-      fact = nplin.norm(v,np.inf)*nplin.norm(C.todense(), np.inf, None)
+      fact = nplin.norm(v,np.inf)*np.max(np.abs(C)*np.ones((n,1)))
 
       # If factor is too large try new shift
       if fact > 1e-2/np.spacing(1):
@@ -194,6 +194,7 @@ def sptarn(A, B, lb, ub, tolconv = 100*np.spacing(1), jmax = 100, maxmul = 10):
 
       ntries = ntries + 1
 
+   print('STARTING')
    # Could not find a suitable shift
    if ntries == maxtries:
       raise RuntimeError("Could not find a suitable shift!")
@@ -212,77 +213,73 @@ def sptarn(A, B, lb, ub, tolconv = 100*np.spacing(1), jmax = 100, maxmul = 10):
    # nt number of converged values (inside and outside)
    nt = 0
    # US is n*nt matrix of Schur vectors
-   US = np.matrix([])
-   # T is nt*nt upper triangular Schur form
-   T = np.matrix([])
+   US = np.empty((n,0))
 
    # Starting vector
    vstart = F(B*np.random.randn(n,1))
+   vstart = F(B*np.ones((n,1)))
 
    terminatealg = False
    ################## CLEAN UP TO HERE ######################
    for mul in range(maxmul):
       # Orthogonalize starting vector against converged vectors
-      if US.shape[1] > 0:
-         v = np.matrix(np.hstack((US, ortho_gs(vstart, US)[0])))
-      else:
-         v = np.matrix(vstart)
+      v = np.hstack((US, ortho_gs(vstart, US)[0]))
 
-      if T.shape[1] > 0:
-         h = np.matrix(np.vstack((T, np.zeros((1,nt)))))
-      else:
-         h = np.matrix(np.zeros((1,nt)))
-      convrun = 0
+      try:
+         h = np.vstack((T, np.zeros((1,nt))))
+      except NameError:
+         h = np.zeros((1,nt))
+
+      convrun = False
 
       # First testing point
-      jt = min(nt+jf,jmax)
+      jt = min(nt+jf, jmax)
       for j in range(nt+1,jmax+1):
-         r = F(B*(v[:,j-1]))
+         r = F(B*(v[:,j-1:j]))
          vj1, hj = ortho_gs(r, v)
-         if h.shape[1] > 0:
-            h = np.hstack((h, hj[0:j-1]))
-         else:
-            h = hj[0:j-1]
+         h = np.hstack((h, hj[0:j]))
          normhj = max(normhj, nplin.norm(hj))
          # Test for linear dependence
          if hj[j] < tol*normhj:
-            convrun = 1
+            convrun = True
             jt = j
          else:
             v = np.hstack((v, vj1))
          
-         h = np.vstack((h, np.hstack((np.zeros((1,j)), hj[j]))))
+         h = np.vstack((h, np.hstack((np.zeros((1,j-1)), hj[j:j+1,:]))))
 
          # One vector added, time to test?
          if j == jt:
-            iv = range(nt,jt)
-            print(iv)
+            iv = np.s_[nt:jt]
             ts, us = splin.schur(h[iv,iv], output='real')
-            tc = splin.rsf1csf(ts, us)[0]
+            tc = splin.rsf2csf(ts, us)[0]
             lms = shift + 1/np.diag(tc)
-            sij = np.abs((h[j+1,j]/normhj)*us[j-nt,:].conj().T)
+            sij = np.abs((h[j,j-1]/normhj)*us[j-nt-1,:]).T
 
             convoutside = np.logical_and(np.logical_or(lms <= lb, lms > ub) , sij <= tolstab)
-            nonconvinside = np.logical_and(np.logical_and(lb < lmb, lms <= ub), np.logical_and(sij > tol, np.abs(us[1,:]).conj().T > tolspur))
-            convrun = np.logical_or(convrun , np.logical_and(np.any(convoutside) and not np.any(nonconvinside)))
+            nonconvinside = np.logical_and(np.logical_and(lb < lms, lms <= ub), np.logical_and(sij > tol, np.abs(us[0,:]).T > tolspur))
+            convrun = convrun or (np.any(convoutside) and not np.any(nonconvinside))
 
             if np.any(nonconvinside):
-               incinside = np.nonzero(nonconvinside)
-               iintr = incinside[1]
+               incinside = np.nonzero(nonconvinside)[0]
+               iintr = incinside[0]
             else:
                iintr = 1
 
-            print('HELLLO???')
+            print('\t\tBasis = ' + str(j) + ' New converged eig = ' + str(np.sum(sij<=tol)))
             jt = min(jt+js,jmax)
          if convrun:
             break
 
       nc = np.sum(np.cumprod(sij<=tol))
-      print('PROGREASSS')
+      print('End of sweep:\tBasis = ' + str(j) + ' New converged eig = ' + str(nc))
 
       if nc > 0:
-         T = np.vstack((np.hstack((T, h[0:nt,iv]*us[:,0:nc-1])), np.hstack((np.zeros((nc,nt)), ts[0:nc,0:nc-1]))))
-         US = np.hstack(US, v[:,iv]*us[:,0:nc-1])
+         try:
+            T = np.vstack((np.hstack((T, h[0:nt,iv].dot(us[:,0:nc]))), np.hstack((np.zeros((nc,nt)), ts[0:nc,0:nc]))))
+         except NameError:
+            T = np.vstack((h[0:nt,iv].dot(us[:,0:nc]), np.hstack((np.zeros((nc,nt)), ts[0:nc,0:nc]))))
+         US = np.hstack((US, v[:,iv].dot(us[:,0:nc])))
          nt = nt + nc
          if nt >= jmax:
             terminatealg = True
@@ -292,7 +289,7 @@ def sptarn(A, B, lb, ub, tolconv = 100*np.spacing(1), jmax = 100, maxmul = 10):
       if nc > 0 and np.any(np.logical_and(newinside ,sij <= tol)):
          vstart = F(B*np.random.randn(n,1))
       elif np.any(newinside):
-         vstart = v[:,iv]*us[:,np.nonzero(newinside)[0]]
+         vstart = v[:,iv].dot(us[:,np.nonzero(newinside)[0][0]])
       else:
          terminatealg = True
 
@@ -305,17 +302,24 @@ def sptarn(A, B, lb, ub, tolconv = 100*np.spacing(1), jmax = 100, maxmul = 10):
       if terminatealg:
          break
    
-   s, dt = nplin.eig(np.multiply(T,(np.abs(T)>tol*normhj)),'nobalance')
-   ev = shift + 1/np.diag(dt)
+   dt, s = nplin.eig(np.multiply(T,(np.abs(T)>tol*normhj)))
+   ev = shift + 1.0/dt
 
-   inside = np.nonzero(np.logical_and(lb < ev ,ev >= ub))
+   inside = np.nonzero(np.logical_and(lb < np.real(ev) ,np.real(ev) <= ub))[0]
    iresult = len(inside)
-   ievsrt = np.argsort(ev[inside])
-   lmb = ev[ievsrt]
-   xv = US*s[:,inside[ievsrt]]
+   if iresult > 0:
+      ev = ev[inside]
+      ievsrt = np.argsort(ev)
+      lmb = ev[ievsrt]
+      xv = US.dot(s[:,inside[ievsrt]])
+   else:
+      lmb = np.array([])
+      xv = np.array([[]])
 
    if not terminatealg:
       iresult = -iresult
+
+   return (xv, lmb, iresult)
 
 
 def choose_shift(lb, ub, perturb = False):
@@ -347,6 +351,7 @@ def choose_shift(lb, ub, perturb = False):
             shift = lb + np.abs(lb)*np.random.rand()
       else:
          shift = lb
+
    # Pick random shift within range
    else:
       shift = (ub - lb)*np.random.rand() + lb
@@ -368,9 +373,9 @@ def ortho_gs(v, mat):
 
    if j > 0:
       for iort in range(maxort):
-         hv = mat.conj().T*v
-         hc = hc + hv
-         v = v - mat*hv
+         hv = mat.conj().T.dot(v)
+         hc = hc[:,0:1] + hv[:,0:1]
+         v = v - mat.dot(hv)
          vnorm = vnn
          vnn = nplin.norm(v)
          # Check for reorthogonalization
@@ -379,6 +384,6 @@ def ortho_gs(v, mat):
 
    # Build orthogonalization coefficients
    h = np.vstack((hc, vnn))
-   y = v/vnn
+   y = np.reshape(v/vnn, (-1,1))
 
    return (y, h)
