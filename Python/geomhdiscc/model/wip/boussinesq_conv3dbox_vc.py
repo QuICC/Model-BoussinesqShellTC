2848476c52669e2b9ab967237e10a2a9cc270d3d
Philippe Marti (garulfo) <philippe.marti@colorado.edu>
2014-08-25 22:46:31 -0600

    More tests on 3D case

--- Python/geomhdiscc/model/wip/boussinesq_conv3dbox_vc.py
+++ Python/geomhdiscc/model/wip/boussinesq_conv3dbox_vc.py
@@ -322,66 +322,63 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
         Pr = eq_params['prandtl']
         Ra = eq_params['rayleigh']
 
-        zero_x = c3d.zblk(res[0], res[1], res[2], 0, 0, 0, no_bc())
-        zero_x = spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
-#        zero_x = zero_x + spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
-#        zero_x = zero_x + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
-#        zero_x = zero_x + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
-        idx_x = (np.ravel(zero_x.sum(axis=1)) > 0)
-        zero_x = spsp.lil_matrix(zero_x.shape)
-        zero_x[idx_x,idx_x] = 1
+#        zero_u = c3d.zblk(res[0], res[1], res[2], 1,1,1, no_bc())
+        zero_u = spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+#        zero_u = zero_u + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+#        zero_u = zero_u + spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+#        zero_u = zero_u + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+        idx_u = (np.ravel(zero_u.sum(axis=1)) > 0)
+        zero_u = spsp.lil_matrix(zero_u.shape)
+        zero_u[idx_u,idx_u] = 1
 
-        zero_y = c3d.zblk(res[0], res[1], res[2], 0, 0, 0, no_bc())
-        zero_y = spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_y = zero_y + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_y = zero_y + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_y = zero_y + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-        idx_y = (np.ravel(zero_y.sum(axis=1)) > 0)
-        zero_y = spsp.lil_matrix(zero_y.shape)
-        zero_y[idx_y,idx_y] = 1
+#        zero_v = c3d.zblk(res[0], res[1], res[2], 1,1,1, no_bc())
+        zero_v = spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_v = zero_v + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_v = zero_v + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_v = zero_v + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+        idx_v = (np.ravel(zero_v.sum(axis=1)) > 0)
+        zero_v = spsp.lil_matrix(zero_v.shape)
+        zero_v[idx_v,idx_v] = 1
 
-        zero_z = c3d.zblk(res[0], res[1], res[2], 0, 0, 0, no_bc())
-        zero_z = spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_z = zero_z + spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_z = zero_z + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_z = zero_z + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-        idx_z = (np.ravel(zero_z.sum(axis=1)) > 0)
-        zero_z = spsp.lil_matrix(zero_z.shape)
-        zero_z[idx_z,idx_z] = 1
+#        zero_w = c3d.zblk(res[0], res[1], res[2], 1,1,1, no_bc())
+        zero_w = spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_w = zero_w + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_w = zero_w + spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_w = zero_w + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+        idx_w = (np.ravel(zero_w.sum(axis=1)) > 0)
+        zero_w = spsp.lil_matrix(zero_w.shape)
+        zero_w[idx_w,idx_w] = 1
 
-        zero_p = c3d.zblk(res[0], res[1], res[2], 0, 0, 0, no_bc())
-#        zero_p = spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
-#        zero_p = spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
-#        zero_p = spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
-#        zero_p = spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+        # Pressures looses all field entries
+#        zero_p = c3d.zblk(res[0], res[1], res[2], 1,1,1, no_bc())
+        zero_p = zero_u + zero_v + zero_w
 
-        # Highest 3 cube
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+        # Highest 3 2D faces
+        zero_p = zero_p + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
+        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
+        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-3, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
 
-        # Highest in each direction
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],0, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
-#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2], 0, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2], res[2]-1, c1d.c1dbc.no_bc()), c1d.qid(res[0], 0, c1d.c1dbc.no_bc())))
+#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2], 0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], 0, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2], res[2]-3, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
+#        zero_p = zero_p + spsp.kron(c1d.qid(res[1], res[1]-3, c1d.c1dbc.no_bc()), spsp.kron(c1d.qid(res[2], 0, c1d.c1dbc.no_bc()), c1d.qid(res[0], res[0]-3, c1d.c1dbc.no_bc())))
         
-        # 0,0,0 mode
-#        zero_p = zero_p + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
+        # Constant pressure
+        zero_p = zero_p + spsp.kron(c1d.sid(res[1], res[1]-1, c1d.c1dbc.no_bc()), spsp.kron(c1d.sid(res[2],res[2]-1, c1d.c1dbc.no_bc()), c1d.sid(res[0], res[0]-1, c1d.c1dbc.no_bc())))
 
-        zero_p = zero_p
         idx_p = (np.ravel(zero_p.sum(axis=1)) > 0)
         zero_p = spsp.lil_matrix(zero_p.shape)
         zero_p[idx_p,idx_p] = 1
+        print(sum(idx_p))
 
         bc = self.convert_bc(eq_params,eigs,bcs,field_row,field_col)
         if field_row == ("velocityx",""):
             if field_col == ("velocityx",""):
                 mat = c3d.i2j2k2lapl(res[0], res[1], res[2], bc)
-                mat[idx_x,:] = 0
-                mat[:,idx_x] = 0
-                mat = mat + zero_x
+                mat[idx_u,:] = 0
+                mat[:,idx_u] = 0
+                mat = mat + zero_u
 
             elif field_col == ("velocityy",""):
                 mat = c3d.zblk(res[0], res[1], res[2], 2, 2, 2, bc)
@@ -403,9 +400,9 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
 
             elif field_col == ("velocityy",""):
                 mat = c3d.i2j2k2lapl(res[0], res[1], res[2], bc)
-                mat[idx_y,:] = 0
-                mat[:,idx_y] = 0
-                mat = mat + zero_y
+                mat[idx_v,:] = 0
+                mat[:,idx_v] = 0
+                mat = mat + zero_v
 
             elif field_col == ("velocityz",""):
                 mat = c3d.zblk(res[0], res[1], res[2], 2, 2, 2, bc)
@@ -427,13 +424,13 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
 
             elif field_col == ("velocityz",""):
                 mat = c3d.i2j2k2lapl(res[0], res[1], res[2], bc)
-                mat[idx_z,:] = 0
-                mat[:,idx_z] = 0
-                mat = mat + zero_z
+                mat[idx_w,:] = 0
+                mat[:,idx_w] = 0
+                mat = mat + zero_w
 
             elif field_col == ("temperature",""):
                 mat = c3d.i2j2k2(res[0], res[1], res[2], bc, Ra/16.0).tolil()
-                mat[idx_z,:] = 0
+                mat[idx_w,:] = 0
 
             elif field_col == ("pressure",""):
                 mat = c3d.i2j2k2f1(res[0], res[1], res[2], bc, -1.0).tolil()
@@ -449,7 +446,7 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
 
             elif field_col == ("velocityz",""):
                 mat = c3d.i2j2k2(res[0], res[1], res[2], bc).tolil()
-                mat[:,idx_z] = 0
+                mat[:,idx_w] = 0
 
             elif field_col == ("temperature",""):
                 mat = c3d.i2j2k2lapl(res[0], res[1], res[2], bc)
@@ -466,7 +463,7 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
                 bc['z']['rt'] = 1
                 bc['z']['cr'] = 1
                 mat = c3d.i1j1k1d1(res[0]+1, res[1]+1, res[2]+1, bc).tolil()
-                mat[:,idx_x] = 0
+                mat[:,idx_u] = 0
                 mat[idx_p,:] = 0
 
             elif field_col == ("velocityy",""):
@@ -477,7 +474,7 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
                 bc['z']['rt'] = 1
                 bc['z']['cr'] = 1
                 mat = c3d.i1j1k1e1(res[0]+1, res[1]+1, res[2]+1, bc).tolil()
-                mat[:,idx_y] = 0
+                mat[:,idx_v] = 0
                 mat[idx_p,:] = 0
 
             elif field_col == ("velocityz",""):
@@ -488,7 +485,7 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
                 bc['z']['rt'] = 1
                 bc['z']['cr'] = 1
                 mat = c3d.i1j1k1f1(res[0]+1, res[1]+1, res[2]+1, bc).tolil()
-                mat[:,idx_z] = 0
+                mat[:,idx_w] = 0
                 mat[idx_p,:] = 0
 
             elif field_col == ("temperature",""):
@@ -506,29 +503,29 @@ class BoussinesqConv3DBoxVC(base_model.BaseModel):
         Pr = eq_params['prandtl']
         Ra = eq_params['rayleigh']
 
-        zero_x = c3d.qid(res[0], res[1], res[2], 0, res[1]-1, res[2]-1, no_bc())
-        idx_x = (np.ravel(zero_x.sum(axis=1)) == 1)
-        zero_y = c3d.qid(res[0], res[1], res[2], res[0]-1, 0, res[2]-1, no_bc())
-        idx_y = (np.ravel(zero_y.sum(axis=1)) == 1)
-        zero_z = c3d.qid(res[0], res[1], res[2], res[0]-1, res[1]-1, 0, no_bc())
-        idx_z = (np.ravel(zero_z.sum(axis=1)) == 1)
-        idx_p = np.logical_or(np.logical_or(idx_x, idx_y),idx_z)
+        zero_u = c3d.qid(res[0], res[1], res[2], 0, res[1]-1, res[2]-1, no_bc())
+        idx_u = (np.ravel(zero_u.sum(axis=1)) == 1)
+        zero_v = c3d.qid(res[0], res[1], res[2], res[0]-1, 0, res[2]-1, no_bc())
+        idx_v = (np.ravel(zero_v.sum(axis=1)) == 1)
+        zero_w = c3d.qid(res[0], res[1], res[2], res[0]-1, res[1]-1, 0, no_bc())
+        idx_w = (np.ravel(zero_w.sum(axis=1)) == 1)
+        idx_p = np.logical_or(np.logical_or(idx_u, idx_v),idx_w)
 
         bc = self.convert_bc(eq_params,eigs,bcs,field_row,field_row)
         if field_row == ("velocityx",""):
             mat = c3d.i2j2k2(res[0], res[1], res[2], bc, 1.0/Pr).tolil()
-            mat[idx_x,:] = 0
-            mat[:,idx_x] = 0
+            mat[idx_u,:] = 0
+            mat[:,idx_u] = 0
 
         elif field_row == ("velocityy",""):
             mat = c3d.i2j2k2(res[0], res[1], res[2], bc, 1.0/Pr).tolil()
-            mat[idx_y,:] = 0
-            mat[:,idx_y] = 0
+            mat[idx_v,:] = 0
+            mat[:,idx_v] = 0
 
         elif field_row == ("velocityz",""):
             mat = c3d.i2j2k2(res[0], res[1], res[2], bc, 1.0/Pr).tolil()
-            mat[idx_z,:] = 0
-            mat[:,idx_z] = 0
+            mat[idx_w,:] = 0
+            mat[:,idx_w] = 0
 
         elif field_row == ("temperature",""):
             mat = c3d.i2j2k2(res[0], res[1], res[2], bc)
