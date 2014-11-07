f741198fe8772c9e32dcb48ab11136311a001d5a
Philippe Marti (Janus) <philippe.marti@colorado.edu>
2014-11-07 14:37:24 -0700

    Correct Nusselt for Beta

--- src/IoVariable/NusseltBeta3DQGPerWriter.cpp
+++ src/IoVariable/NusseltBeta3DQGPerWriter.cpp
@@ -93,36 +93,50 @@ namespace IoVariable {
          tIt = it;
       }
 
-      Array nusselt = Array::Zero(2);
+      ArrayI mode = tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->mode(0);
       ArrayZ tmp;
-      MHDComplex zk_;
-      for(int k = 0; k < tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->dim<Dimensions::Data::DAT3D>(); ++k)
+      Array nusselt = Array::Zero(2);
+      if(mode(2) == 0 && mode(3) == 0)
       {
-         if(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT2D>(0,k) == 0)
-         {
-            nusselt(0) += nlIt->second->dom(0).perturbation().point(0,0,k).real();
+         // Heat flux
+         nusselt(0) = nlIt->second->dom(0).perturbation().point(0,0,0).real();
 
-            if(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT3D>(k) <= tIt->second->dom(0).spRes()->sim()->dim(Dimensions::Simulation::SIM2D, Dimensions::Space::SPECTRAL)/2)
-            {
-               zk_ = MHDComplex(0.0, static_cast<MHDFloat>(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT3D>(k)));
-            } else
-            {
-               zk_ = MHDComplex(0.0, static_cast<MHDFloat>(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT3D>(k)) - tIt->second->dom(0).spRes()->sim()->dim(Dimensions::Simulation::SIM2D, Dimensions::Space::SPECTRAL));
-            }
-            tmp = this->mNusseltOp*tIt->second->dom(0).perturbation().profile(0,k);
-            nusselt(1) += (zk_*tmp(0)).real();
-         }
+         // Mean heat
+         tmp = this->mNusseltOp*tIt->second->dom(0).perturbation().profile(0,0);
+         nusselt(1) = tmp(0).real();
       }
+
+//      Array nusselt = Array::Zero(3);
+//      ArrayZ tmp;
+//      MHDComplex zk_;
+//      for(int k = 0; k < tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->dim<Dimensions::Data::DAT3D>(); ++k)
+//      {
+//         if(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT2D>(0,k) == 0)
+//         {
+//            nusselt(0) += nlIt->second->dom(0).perturbation().point(0,0,k).real();
+//
+//            if(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT3D>(k) <= tIt->second->dom(0).spRes()->sim()->dim(Dimensions::Simulation::SIM2D, Dimensions::Space::SPECTRAL)/2)
+//            {
+//               zk_ = MHDComplex(0.0, static_cast<MHDFloat>(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT3D>(k)));
+//            } else
+//            {
+//               zk_ = MHDComplex(0.0, static_cast<MHDFloat>(tIt->second->dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA1D)->idx<Dimensions::Data::DAT3D>(k)) - tIt->second->dom(0).spRes()->sim()->dim(Dimensions::Simulation::SIM2D, Dimensions::Space::SPECTRAL));
+//            }
+//            tmp = this->mNusseltOp*tIt->second->dom(0).perturbation().profile(0,k);
+//            nusselt(1) += tmp(0).real();
+//            nusselt(2) += (zk_*tmp(0)).real();
+//         }
+//      }
 
       // Get the "global" Nusselt number from MPI code
       #ifdef GEOMHDISCC_MPI
-         MPI_Allreduce(MPI_IN_PLACE, nusselt.data(), 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+         MPI_Allreduce(MPI_IN_PLACE, nusselt.data(), nusselt.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
       #endif //GEOMHDISCC_MPI
 
       // Check if the workflow allows IO to be performed
       if(FrameworkMacro::allowsIO())
       {
-         this->mFile << std::setprecision(16) << this->mTime << "\t" << 1.0 + nusselt(0)/(1 - nusselt(1)) << std::endl;
+         this->mFile << std::setprecision(16) << this->mTime << "\t" << 1.0 - nusselt(0) << "\t" << nusselt(1) << std::endl;
       }
 
       // Close file
