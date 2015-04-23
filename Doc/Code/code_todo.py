07dd8f399675b0b705e11d76f673ae8907674b2b
Philippe Marti <philippe.marti@colorado.edu>
2015-04-23 10:32:41 -0600

    Make sure ikog uses python2.7

--- Doc/Code/code_todo.py
+++ Doc/Code/code_todo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Run the script for details of the licence
