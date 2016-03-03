a28c4fb773d8fda0bd1d61e07aa7ef2cb0c52d5e
Philippe Marti <philippe.marti@colorado.edu>
2016-03-02 17:13:04 -0700

    Updated exact states for sphere

--- src/Generator/States/SphereExactScalarState.cpp
+++ src/Generator/States/SphereExactScalarState.cpp
@@ -96,17 +96,19 @@ namespace Equations {
             nTh = this->unknown().dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA3D)->dim<Dimensions::Data::DAT2D>(iR);
             for(int iTh = 0; iTh < nTh; ++iTh)
             {
-               funcR = 1.0;
-               for(int n = 1; n < 10; ++n)
-               {
-                  funcR += std::pow(r,n);
-               }
-
                theta = thGrid(this->unknown().dom(0).spRes()->cpu()->dim(Dimensions::Transform::TRA3D)->idx<Dimensions::Data::DAT2D>(iTh, iR));
                for(it = modeRange.first; it != modeRange.second; ++it)
                {
                   int l = std::tr1::get<0>(*it);
                   int m = std::tr1::get<1>(*it);
+
+                  funcR = 100.0;
+                  for(int n = 1; n < 63; ++n)
+                  {
+                     funcR += std::pow(r,2*n);
+                  }
+                  funcR *= std::pow(r,l);
+
                   MHDComplex amplitude = std::tr1::get<2>(*it);
 
                   // Spherical harmonic Y_l^m
