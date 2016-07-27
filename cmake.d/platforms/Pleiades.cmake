1f63772a9d006e4351f15ed641f64b66ba685737
Philippe Marti <philippe.marti@colorado.edu>
2016-07-27 11:56:36 -0600

    Updated Pleiades platform file for Meredith account

--- cmake.d/platforms/Pleiades.cmake
+++ cmake.d/platforms/Pleiades.cmake
@@ -26,8 +26,8 @@ set(GEOMHDISCC_THREADSMODELS "None" PARENT_SCOPE)
 
 set(GEOMHDISCC_FFTS "FFTW" PARENT_SCOPE)
 set(GEOMHDISCC_LIBRARIES_FFTW "fftw3" PARENT_SCOPE)
-set(GEOMHDISCC_INCLUDES_FFTW "/nobackupp9/nfeather/marti/libs/include" PARENT_SCOPE)
-set(GEOMHDISCC_LIBDIR_FFTW "/nobackupp9/nfeather/marti/libs/lib" PARENT_SCOPE)
+set(GEOMHDISCC_INCLUDES_FFTW "/nobackup/mplumle1/libs/include" PARENT_SCOPE)
+set(GEOMHDISCC_LIBDIR_FFTW "/nobackup/mplumle1/libs/lib" PARENT_SCOPE)
 
 ###################################################
 #-------------- AVAILABLE FFT PLANS --------------#
@@ -46,13 +46,13 @@ set(GEOMHDISCC_LINALGS "Eigen" PARENT_SCOPE)
 ###################################################
 
 set(GEOMHDISCC_SPLINALGS "MUMPS" "SparseLU" "UmfPack" PARENT_SCOPE)
-set(GEOMHDISCC_INCLUDES_MUMPS "/nobackupp9/nfeather/marti/libs/include" PARENT_SCOPE)
-set(GEOMHDISCC_LIBDIR_MUMPS "/nobackupp9/nfeather/marti/libs/lib" "$ENV{MKLROOT}/lib/intel64" PARENT_SCOPE)
+set(GEOMHDISCC_INCLUDES_MUMPS "/nobackup/mplumle1/libs/include" PARENT_SCOPE)
+set(GEOMHDISCC_LIBDIR_MUMPS "/nobackup/mplumle1/libs/lib" "$ENV{MKLROOT}/lib/intel64" PARENT_SCOPE)
 #set(GEOMHDISCC_LIBRARIES_MUMPS "dmumps" "zmumps" "mumps_common" "pord" "parmetis" "mkl_scalapack_lp64" "mkl_intel_lp64" "mkl_sequential" "mkl_core" "pthread" "m" "mpifort" "ifcore" PARENT_SCOPE)
 set(GEOMHDISCC_LIBRARIES_MUMPS "dmumps" "zmumps" "mumps_common" "pord" "parmetis" "metis" "mkl_scalapack_lp64" "mkl_intel_lp64" "mkl_core" "mkl_sequential" "mkl_blacs_sgimpt_lp64" "pthread" "m" "dl" "ifcore" PARENT_SCOPE)
 set(GEOMHDISCC_LIBRARIES_UMFPACK "mkl_intel_lp64" "mkl_sequential" "mkl_core" "pthread" "m" "umfpack" "amd" "cholmod" "ccolamd" "colamd" "camd" "metis" "suitesparseconfig" PARENT_SCOPE)
-set(GEOMHDISCC_INCLUDES_UMFPACK_INTEL "$ENV{HOME}/local/include" PARENT_SCOPE)
-set(GEOMHDISCC_LIBDIR_UMFPACK_INTEL $ENV{TACC_MKL_LIB} "$ENV{HOME}/local/lib" PARENT_SCOPE)
+set(GEOMHDISCC_INCLUDES_UMFPACK_INTEL "/nobackup/mplumle1/libs/include" PARENT_SCOPE)
+set(GEOMHDISCC_LIBDIR_UMFPACK_INTEL "$ENV{MKLROOT}/lib/intel64" "/nobackup/mplumle1/libs/lib" PARENT_SCOPE)
 
 ###################################################
 #- AVAILABLE SPARSE SPD LINEAR ALGEBRA LIBRARIES -#
