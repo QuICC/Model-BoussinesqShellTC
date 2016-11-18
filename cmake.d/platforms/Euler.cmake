c9081372be22d878790b25a2eb4a13e2e4e1eb56
Nicolo Lardelli <nicolol@euler05.hpc-lca.ethz.ch>
2016-11-18 09:16:16 +0100

    State generation of CouetteShell reverted to non-random and -rpath added to Euler.cmake

--- cmake.d/platforms/Euler.cmake
+++ cmake.d/platforms/Euler.cmake
@@ -59,7 +59,7 @@ set(GEOMHDISCC_SPSPDLINALGS "SimplicialLDLT" "SimplicialLLT" "MUMPS" "UmfPack" P
 set(GEOMHDISCC_LIBRARIES_MUMPS "dmumps" "zmumps" "mumps_common" "metis" "openblas" "goto2" "mpi" "gfortran" "mpi_mpifh" "cmumps" "smumps" "pord" "scalapack" "parmetis" PARENT_SCOPE)
 #set(GEOMHDISCC_LIBRARIES_MUMPS "dmumps" "zmumps" "mumps_common" "parmetis" "ptesmumps" "ptscotch" "ptscotcherr" "ptscotchparmetis" "ptscotcherrexit" "scotch" "scotcherr" "scotcherrexit" "pord" "scalapack" "metis" "atllapack" "f77blas" "mpi" "gfortran" "mpi_mpifh" PARENT_SCOPE)
 set(GEOMHDISCC_INCLUDES_MUMPS "/cluster/apps/mumps/5.0.1/x86_64/gcc_4.8.2/openmpi_1.6.5/include" PARENT_SCOPE)
-set(GEOMHDISCC_LIBDIR_MUMPS "/cluster/apps/mumps/5.0.1/x86_64/gcc_4.8.2/openmpi_1.6.5/lib" PARENT_SCOPE)
+set(GEOMHDISCC_LIBDIR_MUMPS "/cluster/apps/mumps/5.0.1/x86_64/gcc_4.8.2/openmpi_1.6.5/lib" "/cluster/apps/openmpi/1.6.5/x86_64/gcc_4.8.2/lib" PARENT_SCOPE)
 
 ###################################################
 #- AVAILABLE SPARSE TRI LINEAR ALGEBRA LIBRARIES -#
@@ -103,7 +103,7 @@ set(GEOMHDISCC_CC_INC_GCC "" PARENT_SCOPE)
 
 set(GEOMHDISCC_CC_INC_MPI_GCC "${GEOMHDISCC_CC_INC_GCC}" PARENT_SCOPE)
 
-set(GEOMHDISCC_CC_LIB_GCC "" PARENT_SCOPE)
+set(GEOMHDISCC_CC_LIB_GCC "-rpath" PARENT_SCOPE)
 
 set(GEOMHDISCC_CC_LIB_MPI_GCC "${GEOMHDISCC_CC_LIB_GCC}" PARENT_SCOPE)
 
