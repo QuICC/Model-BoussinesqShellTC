9eabe6c826e5000c99b271af6c62360ad10d004d
nicolol <nicolol@erdw.ethz.ch>
2017-01-11 13:48:11 +0100

    commit

--- Scripts/Python/represent_specta.py
+++ Scripts/Python/represent_specta.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as pp
 import sys
 import pandas as pd
+from geomhdiscc.geometry.spherical import shell_radius
 
 if __name__=="__main__":
 
@@ -17,11 +18,32 @@ if __name__=="__main__":
 
     file = h5py.File(filename, 'r')
 
+    # call linear_r2x
+    ro = file['physical/ro'].value
+    rratio = file['physical/rratio'].value
+    a, b = shell_radius.linear_r2x(ro, rratio)
+
+    # get the matrices
+    nr = file['truncation/spectral/dim1D'].value
+    bc = dict()
+    bc[0]=0
+    R2 = shell_radius.r2(nr, a, b, bc)
+    Integral = shell_radius.integral(nr, a, b)
+
+    print(R2.todense())
+    print(Integral.todense())
+    raise RuntimeError
+
     pp.figure()
     to_select = ['velocity/velocity_tor', 'velocity/velocity_pol']
     df = pd.DataFrame()
     for field in to_select:
         a = file[field][:]
+
+        for aa in a:
+            print(np.dot(Integral,aa))
+            print(np.dot(R2,aa))
+            raise RuntimeError
         vec = (a**2).sum(1).sum(1)
         index = 0
         lmax = int((vec.shape[0]*2+.25)**.5-.5)
