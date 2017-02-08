58498baad2d8fc2b874986a51ed159213cde0820
Philippe Marti <philippe.marti@echinops.ch>
2017-02-08 09:10:10 +0100

    Add preliminary RBC in plane layer model

---
+++ include/Equations/Box/Boussinesq/BoussinesqRBCPlaneMomentum.hpp
@@ -0,0 +1,78 @@
+/**
+ * @file BoussinesqRBCPlaneMomentum.hpp
+ * @brief Implementation of the vector momentum equation for Rayleigh-Benard convection in a plane layer (toroidal/poloidal formulation)
+ * @author Philippe Marti \<philippe.marti@colorado.edu\>
+ */
+
+#ifndef BOUSSINESQRBCPLANEMOMENTUM_HPP
+#define BOUSSINESQRBCPLANEMOMENTUM_HPP
+
+// Configuration includes
+//
+#include "SmartPointers/SharedPtrMacro.h"
+
+// System includes
+//
+
+// External includes
+//
+
+// Project includes
+//
+#include "Base/Typedefs.hpp"
+#include "TypeSelectors/ScalarSelector.hpp"
+#include "Equations/IVectorEquation.hpp"
+
+namespace QuICC {
+
+namespace Equations {
+
+   /**
+    * @brief Implementation of the vector momentum equation for Rayleigh-Benard convection in a plane layer (toroidal/poloidal formulation)
+    */
+   class BoussinesqRBCPlaneMomentum: public IVectorEquation
+   {
+      public:
+         /**
+          * @brief Simple constructor
+          *
+          * @param spEqParams    Shared equation parameters
+          */
+         BoussinesqRBCPlaneMomentum(SharedEquationParameters spEqParams);
+
+         /**
+          * @brief Simple empty destructor
+          */
+         virtual ~BoussinesqRBCPlaneMomentum();
+
+         /**
+          * @brief Compute the nonlinear interaction term
+          *
+          * @param rNLComp Nonlinear term component
+          * @param id      ID of the component (allows for a more general implementation)
+          */
+         virtual void computeNonlinear(Datatypes::PhysicalScalarType& rNLComp, FieldComponents::Physical::Id id) const;
+
+      protected:
+         /**
+          * @brief Set variable requirements
+          */
+         virtual void setRequirements();
+
+         /**
+          * @brief Set the equation coupling information
+          */
+         virtual void setCoupling();
+
+         /**
+          * @brief Set the nonlinear integration components
+          */
+         virtual void setNLComponents();
+
+      private:
+   };
+
+}
+}
+
+#endif // BOUSSINESQRBCPLANEMOMENTUM_HPP
