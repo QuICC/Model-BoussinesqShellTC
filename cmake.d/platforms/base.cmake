f8008a407f6ebf97e617c4fe0e17eb6bf705c1d8
Leonardo Echeverria <leonardo.echeverria@gmail.com>
2017-03-24 15:35:49 +0100

    changing base.cmake to list files in OSX

--- cmake.d/platforms/base.cmake
+++ cmake.d/platforms/base.cmake
@@ -3,8 +3,19 @@
 ###################################################
 
 # Get list of platforms
-file(GLOB Platforms
-   ${CMAKE_SOURCE_DIR}/cmake.d/platforms/[A-Z]*.cmake)
+# Leo: this doesn't work 
+
+IF(APPLE)
+    file(GLOB Platforms
+        ${CMAKE_SOURCE_DIR}/cmake.d/platforms/*.cmake)
+ELSE()
+    file(GLOB Platforms
+        ${CMAKE_SOURCE_DIR}/cmake.d/platforms/[A-Z]*.cmake)
+ENDIF()
+
+    #IF (APPLE)
+    #ENDIF (APPLE)
+
 
 # Cleanup list
 foreach(Platform ${Platforms})
