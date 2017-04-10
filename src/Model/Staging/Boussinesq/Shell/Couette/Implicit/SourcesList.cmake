4c77dd67e00cb8547d872ebaaf4062df7ac2ea47
nicolol <nicolol@erdw.ethz.ch>
2017-04-10 13:51:26 +0200

    added the files for implicit couette, still not working

---
+++ src/Model/Staging/Boussinesq/Shell/Couette/Implicit/SourcesList.cmake
@@ -0,0 +1,8 @@
+set(MHDSources
+   PhysicalModel.cpp
+   ../../../../../../IoVariable/ShellTorPolTracerWriter.cpp
+   ../../../../../../IoVariable/ShellTorPolTorqueWriter.cpp
+)
+
+set(MHDSrcSubDirs
+)
