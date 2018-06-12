de30462fba36fd3ed694f03c9338db5914741a4b
nicolol <nicolol@erdw.ethz.ch>
2018-06-12 17:17:37 +0200

    more ascii output

--- src/Model/Staging/Boussinesq/Shell/CouetteDynamo/Explicit/SourcesList.cmake
+++ src/Model/Staging/Boussinesq/Shell/CouetteDynamo/Explicit/SourcesList.cmake
@@ -2,6 +2,7 @@ set(MHDSources
    PhysicalModel.cpp
    ../../../../../../IoVariable/ShellTorPolProbeWriter.cpp
    ../../../../../../IoVariable/ShellTorPolTorqueWriter.cpp
+   ../../../../../../IoVariable/ShellTorPolAverageVorticityWriter.cpp
 )
 
 set(MHDSrcSubDirs
