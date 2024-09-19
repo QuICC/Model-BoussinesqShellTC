5ea5f8862f0cbd77736448b4fd24e1b1ff747509
Philippe Marti <philippe.marti@erdw.ethz.ch>
2024-09-19 08:42:42 +0200

    Fix split equation formulation

--- Model/Boussinesq/Shell/TC/Explicit/ModelBackend.cpp
+++ Model/Boussinesq/Shell/TC/Explicit/ModelBackend.cpp
@@ -766,7 +766,7 @@ void ModelBackend::modelMatrix(DecoupledZSparse& rModelMatrix,
             auto colId = *pColId;
             auto descr = splitBoundaryValueBlockBuilder(rowId, colId, res, eigs,
                bcs, nds);
-            buildBlock(rModelMatrix, descr, rowId, colId, fields, matIdx,
+            buildFixedBlock(rModelMatrix, 2, true, descr, rowId, colId, fields, matIdx,
                bcType, res, l, l, bcs, nds, false);
          }
       }
